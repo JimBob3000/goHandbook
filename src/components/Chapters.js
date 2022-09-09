82cc357a7cdb5d3d44c08df13ac2deeb5c85b435
JimBob3000 <jpelliott599@hotmail.co.uk>
2022-09-09 19:52:56 +0100

    Removing unused chapters

--- src/components/Chapters.js
+++ src/components/Chapters.js
@@ -33,46 +33,6 @@ const Chapters = () => {
                     <li className="leading-9"><a className="inactive" href="/searching#binarySearch">Binary Search</a></li>
                 </ul>
             </li>
-            <li>
-                <h5 className="lg:mb-3 font-semibold text-slate-800 dark:text-slate-200">Chapter</h5>
-                <ul className="mt-4  border-l-2 border-slate-100">
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                </ul>
-            </li>
-            <li>
-                <h5 className="lg:mb-3 font-semibold text-slate-800 dark:text-slate-200">Chapter</h5>
-                <ul className="mt-4  border-l-2 border-slate-100">
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                </ul>
-            </li>
-            <li>
-                <h5 className="lg:mb-3 font-semibold text-slate-800 dark:text-slate-200">Chapter</h5>
-                <ul className="mt-4  border-l-2 border-slate-100">
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                </ul>
-            </li>
-            <li>
-                <h5 className="lg:mb-3 font-semibold text-slate-800 dark:text-slate-200">Chapter</h5>
-                <ul className="mt-4  border-l-2 border-slate-100">
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                </ul>
-            </li>
-            <li>
-                <h5 className="lg:mb-3 font-semibold text-slate-800 dark:text-slate-200">Chapter</h5>
-                <ul className="mt-4  border-l-2 border-slate-100">
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                    <li className="leading-9"><a className="inactive" href="/">Section</a></li>
-                </ul>
-            </li>
         </ul>
     );
 }
