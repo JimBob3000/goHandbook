4b78bd0fbcc79c4d1291eccaa404645d7094dbab
JimBob3000 <jpelliott599@hotmail.co.uk>
2022-09-15 16:17:07 +0100

    Refactoring Topic page selection

--- src/pages/topics/Topics.js
+++ src/pages/topics/Topics.js
@@ -6,37 +6,40 @@ import Structs from "./components/Structs";
 import Searching from "./components/Searching";
 import Sorting from "./components/Sorting";
 import { useState } from 'react';
+import { useCallback } from 'react';
 
 const Topics = ({topic, subTopic}) => {
     const [currentTopic, setCurrentTopic] = useState(topic)
     const [currentSubTopic, setCurrentSubTopic] = useState(subTopic)
-    const topics = [
-        "loops",
-        "structs",
-        "searching",
-        "sorting",
-    ]
 
-    if (topics.includes(currentTopic)) {
-        return ( 
-            <div className="xl:px-auto relative mx-auto flex justify-center lg:justify-start xl:justify-center sm:px-2 lg:px-8">
-                <SideMenu 
-                    currentTopic={currentTopic} 
-                    setCurrentTopic={setCurrentTopic}
-                    currentSubTopic={currentSubTopic} 
-                    setCurrentSubTopic={setCurrentSubTopic}
-                />
-                <div className="px-6 md:px-6 lg:px-0 py-16 text-slate-800 md:w-[700px] lg:w-full lg:ml-[30px] 2xl:ml-[60px] lg:mr-[0px] xl:w-auto xl:max-w-[900px]   mx-auto">
-                    {currentTopic === "loops" && <Loops />}
-                    {currentTopic === "structs" && <Structs />}
-                    {currentTopic === "searching" && <Searching />}
-                    {currentTopic === "sorting" && <Sorting />}
-                </div>
+    const ActiveTopic = useCallback((props) => {
+        switch (currentTopic) {
+            case "loops":
+                return ( <Loops /> );
+            case "structs":
+                return ( <Structs /> );
+            case "searching":
+                return ( <Searching /> );
+            case "sorting":
+                return ( <Sorting /> );
+            default:
+                return ( <NotFound /> );
+        }
+    }, [currentTopic] );
+
+    return (
+        <div className="xl:px-auto relative mx-auto flex justify-center lg:justify-start xl:justify-center sm:px-2 lg:px-8">
+            <SideMenu 
+                currentTopic={currentTopic} 
+                setCurrentTopic={setCurrentTopic}
+                currentSubTopic={currentSubTopic} 
+                setCurrentSubTopic={setCurrentSubTopic}
+            />
+            <div className="px-6 md:px-6 lg:px-0 py-16 text-slate-800 md:w-[700px] lg:w-full lg:ml-[30px] 2xl:ml-[60px] lg:mr-[0px] xl:w-auto xl:max-w-[900px]   mx-auto">
+                <ActiveTopic />
             </div>
-        );
-    } else {
-        return <NotFound />
-    }
+        </div>
+    )
 }
  
 export default Topics;
